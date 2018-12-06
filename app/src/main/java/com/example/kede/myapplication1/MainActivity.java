57861e7ce7ac3bde716e6f402f22bbebf2a8b10f
Clare <lilingjie1992@gmail.com>
2018-12-06 23:21:16 +0800

    activity life state

--- app/src/main/java/com/example/kede/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/kede/myapplication1/MainActivity.java
@@ -10,11 +10,33 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.kede.myapplication1.MESSAGE";
 
+    //1. 必须实现 在系统创建activity时触发 初始化activity必须部分
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //2. 将要显示给用户 包含了变为活跃之前最终准备
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    //3. 仅在用户交互之前调用 此时界面已经显示在屏幕最上面 在这个方法里面处理大多数功能逻辑
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
